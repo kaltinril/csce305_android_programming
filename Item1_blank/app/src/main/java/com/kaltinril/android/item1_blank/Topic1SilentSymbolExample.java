df269a7278ef8c12580d5c75c04d4699e2899c37
Jeremy Swartwood <jeremy.swartwood@gmai.com>
2017-10-16 15:18:20 -0800

    Add sounds and code to play one of the sounds related to the heaven word

--- Item1_blank/app/src/main/java/com/kaltinril/android/item1_blank/Topic1SilentSymbolExample.java
+++ Item1_blank/app/src/main/java/com/kaltinril/android/item1_blank/Topic1SilentSymbolExample.java
@@ -1,16 +1,56 @@
 package com.kaltinril.android.item1_blank;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Topic1SilentSymbolExample extends AppCompatActivity {
+
+    // Store these so that we don't have to re-load them all the time
+    MediaPlayer mp;
+    Button silentButton ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_silent_symbol_example);
+
+        // Get access to the button
+        silentButton = (Button)this.findViewById(R.id.Rec17_1);
+
+        // Build a dynamic listener and attach it to the above button
+        silentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Reset the MediaPlayer object if it is not released.
+                if(mp!=null) {
+                    mp.release();
+                    mp=null;
+                }
+
+                // Setup the Mdeia Player so we can play the sound
+                mp = MediaPlayer.create(Topic1SilentSymbolExample.this, R.raw.rec17_1);
+
+                // Start the Media Player
+                mp.start();
+            }
+        });
+
+    }
+
+    // Make sure we clean up the media player on destroy.
+    @Override
+    protected void onDestroy(){
+        if (mp != null){
+            mp.release();
+            mp = null;
+        }
+
+        super.onDestroy();
     }
 
     public void overviewClick(View view){
