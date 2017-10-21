d7b40c062dfa6d6df05cc291cffa3d938520e423
Jeremy Swartwood <jeremy.swartwood@gmai.com>
2017-10-20 23:02:19 -0800

    Make the app use the lucky color of the day for thailand

--- Item2/app/src/main/java/com/kaltinril/android/item2/MainActivity.java
+++ Item2/app/src/main/java/com/kaltinril/android/item2/MainActivity.java
@@ -1,16 +1,57 @@
 package com.kaltinril.android.item2;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Pick the theme based on the day of the week so we always use the "lucky" color for thai
+        int theme = generateTheme();
+        setTheme(theme);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private int generateTheme(){
+
+        Calendar rightNow = Calendar.getInstance();
+        int day = rightNow.get(Calendar.DAY_OF_WEEK);
+
+        int theme = R.style.AppTheme_Sunday;
+
+        switch(day){
+            case Calendar.SUNDAY:
+                theme = R.style.AppTheme_Sunday;
+                break;
+            case Calendar.MONDAY:
+                theme = R.style.AppTheme_Monday;
+                break;
+            case Calendar.TUESDAY:
+                theme = R.style.AppTheme_Tuesday;
+                break;
+            case Calendar.WEDNESDAY:
+                theme = R.style.AppTheme_Wednesday;
+                break;
+            case Calendar.THURSDAY:
+                theme = R.style.AppTheme_Thursday;
+                break;
+            case Calendar.FRIDAY:
+                theme = R.style.AppTheme_Friday;
+                break;
+            case Calendar.SATURDAY:
+                theme = R.style.AppTheme_Saturday;
+                break;
+        }
+
+        return theme;
     }
 
     public void topic1Click(View view){
